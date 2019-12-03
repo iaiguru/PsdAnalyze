92f711c790f304ac2c3ee83c74e193a9d942e4b8
devninja9 <devninja9@gmail.com>
2019-12-03 23:14:43 +0800

    Add logging

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import os
 from psd_tools import PSDImage
+from loguru import logger
 
 
 def main():
-    imagesetDir = '.'
+    imagesetDir = 'E:\\ImageSet'
     inputExts = ('.psd')
+    logFileName = "log.txt"
 
+    # init logger
+    logger.add(logFileName, format="{time} {level} {message}", level="INFO")
+
+    logger.info('process started')
+    # traverse psd dir
     for dirName, subdirList, fileList in os.walk(imagesetDir):
         for fname in fileList:
             if fname.lower().endswith(inputExts):
                 procFile(dirName, fname)
+
+    logger.info('process completed')
 
 
 def procFile(dirName, fileName):
@@ -24,16 +33,16 @@ def procFile(dirName, fileName):
         psd = PSDImage.open(inputFilePath)
         # export composed layer
         psd.compose().save(targetsetDir + outputFileName)
-        print('Target - ' + inputFilePath)
+        logger.info(fileName + ' -> target')
         # export background layer
         for layer in psd:
             if layer.name == '背景':
                 layer_image = layer.topil()
                 layer_image.save(rawsetDir + outputFileName)
-                print('Raw - ' + inputFilePath)
+                logger.info(fileName + ' -> raw')
                 break
     except:
-        print('Parsing Error - ' + inputFilePath)
+        logger.error(fileName + ' -> error')
 
 
 if __name__ == "__main__":
